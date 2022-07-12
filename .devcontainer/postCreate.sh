48430d455ef1c3e0209b2859b117e8ab01cb042b
Domonkos Papp <51707177+domonkospapp@users.noreply.github.com>
2022-07-12 12:51:54 +0200

    Update postCreate.sh

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+ln -sf $PWD/.devcontainer/config $HOME/config && set +x
 
-terraform init
 
-aws configure set aws_access_key_id $AWS_ACCESS_KEY
-aws configure set aws_secret_access_key $AWS_SECRET
+# terraform init
+
+# aws configure set aws_access_key_id $AWS_ACCESS_KEY
+# aws configure set aws_secret_access_key $AWS_SECRET
