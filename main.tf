4acc66354f392129bc6af9421c6e711386e471e8
Domonkos Papp <51707177+domonkospapp@users.noreply.github.com>
2022-07-31 14:04:33 +0000

    Variables for db resource

--- main.tf
+++ main.tf
@@ -184,9 +184,9 @@ resource "aws_db_instance" "db_instance" {
   engine               = "mysql"
   engine_version       = "5.7"
   instance_class       = "db.t3.micro"
-  name                 = "mydb"
-  username             = "foo"
-  password             = "foobarbaz"
+  name                 = var.db_name
+  username             = var.db_user
+  password             = var.db_pass
   parameter_group_name = "default.mysql5.7"
   skip_final_snapshot  = true
 }
---
+++ variables.tf
@@ -0,0 +1,17 @@
+# RDS Variables
+
+variable "db_name" {
+  description = "Name of DB"
+  type        = string
+}
+
+variable "db_user" {
+  description = "Username for DB"
+  type        = string
+}
+
+variable "db_pass" {
+  description = "Password for DB"
+  type        = string
+  sensitive   = true
+}
