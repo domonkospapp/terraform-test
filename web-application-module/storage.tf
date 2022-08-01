dad83721f7667844f0619110b4b010af32df2f10
Domonkos Papp <51707177+domonkospapp@users.noreply.github.com>
2022-08-01 21:46:48 +0000

    Add storage definition to module

---
+++ web-application-module/storage.tf
@@ -0,0 +1,20 @@
+resource "aws_s3_bucket" "bucket" {
+  bucket = var.bucket_name
+}
+
+resource "aws_s3_bucket_versioning" "versioning_bucket" {
+  bucket = aws_s3_bucket.bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "bucket" {
+  bucket = aws_s3_bucket.bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
