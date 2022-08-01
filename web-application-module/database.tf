0ab0ddcaacd34f8efd90c873a0a3b9c51e7f6cb5
Domonkos Papp <51707177+domonkospapp@users.noreply.github.com>
2022-08-01 21:44:45 +0000

    Add db instance to the module

---
+++ web-application-module/database.tf
@@ -0,0 +1,11 @@
+resource "aws_db_instance" "db_instance" {
+  allocated_storage    = 10
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  name                 = var.db_name
+  username             = var.db_user
+  password             = var.db_pass
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
+}
