93bc9fcff8c71d1ae71a4df637eea3269da50590
Domonkos Papp <51707177+domonkospapp@users.noreply.github.com>
2022-08-02 21:18:23 +0000

    Use module for two infra creation

--- main.tf
+++ main.tf
@@ -5,188 +5,34 @@ terraform {
       name = "first-workspace"
     }
   }
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
 }
 
 provider "aws" {
   region = var.region
 }
 
-resource "aws_instance" "instance_1" {
-  ami             = var.ami
-  instance_type   = var.instance_type
-  security_groups = [aws_security_group.instances.name]
-  user_data       = <<-EOF
-              #!/bin/bash
-              echo "Hello, World 1" > index.html
-              python3 -m http.server 8080 &
-              EOF
+module "web_app_1" {
+  source = "./web-application-module"
+
+  # Input Variables
+  bucket_name      = "${var.bucket_name}-1"
+  app_name         = "app-1"
+  environment_name = "production"
+  # instance_type    = "t2.small"
+  db_name          = var.db_name
+  db_user          = var.db_user
+  db_pass          = var.db_pass_1
 }
 
-resource "aws_instance" "instance_2" {
-  ami             = var.ami
-  instance_type   = var.instance_type
-  security_groups = [aws_security_group.instances.name]
-  user_data       = <<-EOF
-              #!/bin/bash
-              echo "Hello, World 2" > index.html
-              python3 -m http.server 8080 &
-              EOF
-}
+module "web_app_2" {
+  source = "./web-application-module"
 
-resource "aws_s3_bucket" "bucket" {
-  bucket = var.bucket_name
-}
-
-resource "aws_s3_bucket_versioning" "versioning_bucket" {
-  bucket = aws_s3_bucket.bucket.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "bucket" {
-  bucket = aws_s3_bucket.bucket.id
-
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-}
-
-data "aws_vpc" "default_vpc" {
-  default = true
-}
-
-data "aws_subnet_ids" "default_subnet" {
-  vpc_id = data.aws_vpc.default_vpc.id
-}
-
-resource "aws_security_group" "instances" {
-  name = "instance-security-group"
-}
-
-resource "aws_security_group_rule" "allow_http_inbound" {
-  type              = "ingress"
-  security_group_id = aws_security_group.instances.id
-
-  from_port   = 8080
-  to_port     = 8080
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-}
-
-resource "aws_lb_listener" "http" {
-  load_balancer_arn = aws_lb.load_balancer.arn
-
-  port = 80
-  protocol = "HTTP"
-
-  default_action {
-    type = "fixed-response"
-
-    fixed_response {
-      content_type = "text/plain"
-      message_body = "404: page not found"
-      status_code  = 404
-    }
-  }
-}
-
-resource "aws_lb_target_group" "instances" {
-  name     = "example-target-group"
-  port     = 8080
-  protocol = "HTTP"
-  vpc_id   = data.aws_vpc.default_vpc.id
-
-  health_check {
-    path                = "/"
-    protocol            = "HTTP"
-    matcher             = "200"
-    interval            = 15
-    timeout             = 3
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-  }
-}
-
-resource "aws_lb_target_group_attachment" "instance_1" {
-  target_group_arn = aws_lb_target_group.instances.arn
-  target_id        = aws_instance.instance_1.id
-  port             = 8080
-}
-
-resource "aws_lb_target_group_attachment" "instance_2" {
-  target_group_arn = aws_lb_target_group.instances.arn
-  target_id        = aws_instance.instance_2.id
-  port             = 8080
-}
-
-resource "aws_lb_listener_rule" "instances" {
-  listener_arn = aws_lb_listener.http.arn
-  priority     = 100
-
-  condition {
-    path_pattern {
-      values = ["*"]
-    }
-  }
-
-  action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.instances.arn
-  }
-}
-
-
-resource "aws_security_group" "alb" {
-  name = "alb-security-group"
-}
-
-resource "aws_security_group_rule" "allow_alb_http_inbound" {
-  type              = "ingress"
-  security_group_id = aws_security_group.alb.id
-
-  from_port   = 80
-  to_port     = 80
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-
-}
-
-resource "aws_security_group_rule" "allow_alb_all_outbound" {
-  type              = "egress"
-  security_group_id = aws_security_group.alb.id
-
-  from_port   = 0
-  to_port     = 0
-  protocol    = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
-
-}
-
-
-resource "aws_lb" "load_balancer" {
-  name               = "web-app-lb"
-  load_balancer_type = "application"
-  subnets            = data.aws_subnet_ids.default_subnet.ids
-  security_groups    = [aws_security_group.alb.id]
-}
-
-resource "aws_db_instance" "db_instance" {
-  allocated_storage    = 10
-  engine               = "mysql"
-  engine_version       = "5.7"
-  instance_class       = "db.t3.micro"
-  name                 = var.db_name
-  username             = var.db_user
-  password             = var.db_pass
-  parameter_group_name = "default.mysql5.7"
-  skip_final_snapshot  = true
+  # Input Variables
+  bucket_name      = "${var.bucket_name}-2"
+  app_name         = "app-2"
+  environment_name = "production"
+  # instance_type    = "t2.small"
+  db_name          = var.db_name
+  db_user          = var.db_user
+  db_pass          = var.db_pass_2
 }
