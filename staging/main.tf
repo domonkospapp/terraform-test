3a33b1f9343bca23bce7a15296726ec7cd512690
Domonkos Papp <51707177+domonkospapp@users.noreply.github.com>
2022-08-03 19:03:26 +0000

    Create staging environment

---
+++ staging/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  cloud {
+    organization = "domonkospapp"
+    workspaces {
+      name = "staging-workspace"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+locals {
+  environment_name = "staging"
+}
+
+module "web_app" {
+  source = "../web-application-module"
+
+  # Input Variables
+  bucket_name      = "${var.bucket_name}-1"
+  app_name         = "app-1"
+  environment_name = "staging"
+  # instance_type    = "t2.small"
+  db_name          = "${local.environment_name}${var.db_name}"
+  db_user          = var.db_user
+  db_pass          = var.db_pass
+}
