a05ed2590748ffc04b60e49819a93be89ee81b54
Domonkos Papp <51707177+domonkospapp@users.noreply.github.com>
2022-08-05 16:54:56 +0000

    Add infrastructure test

---
+++ test/bash/test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -euo pipefail
+
+cd ../../staging
+
+# Create the resources
+terraform init
+terraform apply -auto-approve -var=db_user='user123' -var=db_pass='password123' -var=db_name='test-db' -var=db_pass='password123' -var=bucket_name='test123'
+
+# Wait while the instance boots up
+# (Could also use a provisioner in the TF config to do this)
+sleep 60 
+
+# Query the output, extract the IP and make a request
+terraform output -json |\
+jq -r '.instance_ip_addr.value' |\
+xargs -I {} curl http://{}:8080 -m 10
+
+# If request succeeds, destroy the resources
+terraform destroy -auto-approve
