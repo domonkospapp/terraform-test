cd03f7752be9c40af6bf2e2d0e29fbfd1926f684
Domonkos Papp <51707177+domonkospapp@users.noreply.github.com>
2022-08-08 21:21:50 +0000

    Add test var file

--- test/bash/test.sh
+++ test/bash/test.sh
@@ -5,7 +5,7 @@ cd ../../staging
 
 # Create the resources
 terraform init
-terraform apply -auto-approve -var=db_user='user123' -var=db_pass='password123' -var=db_name='testdb' -var=db_pass='password123' -var=bucket_name='test321321312'
+terraform apply -auto-approve -var=db_pass="password123" -var-file="../test/bash/test.tfvars"
 
 # Wait while the instance boots up
 # (Could also use a provisioner in the TF config to do this)
@@ -17,4 +17,4 @@ jq -r '.web_app_module.value.load_balancer_dns_name' |\
 xargs -I {} curl http://{} -m 10
 
 # If request succeeds, destroy the resources
-terraform destroy -auto-approve -var=db_user='user123' -var=db_pass='password123' -var=db_name='testdb' -var=db_pass='password123' -var=bucket_name='test321321312'
+terraform destroy -auto-approve -var=db_pass="password123" -var-file="../test/bash/test.tfvars"
---
+++ test/bash/test.tfvars
@@ -0,0 +1,3 @@
+db_user="user123"
+db_name="testdb"
+bucket_name="test123bucket"
